d7b615b658e47f784fc01cf2309fa63bb77e42ee
aayushsapkota01 <sapkotaaayush10@gmail.com>
2025-01-06 08:15:46 -0500

    Complete Album controller functionality

--- server/src/routes/album.routes.js
+++ server/src/routes/album.routes.js
@@ -1,5 +1,13 @@
 import { Router } from "express";
 
+import {
+  getAllAlbums,
+  getAlbumById,
+} from "../controllers/album.controllers.js";
+
 const router = Router();
+
+router.get("/", getAllAlbums);
+router.get("/:albumId", getAlbumById);
 
 export default router;
